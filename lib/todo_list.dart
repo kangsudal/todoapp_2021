0ce96c4e404ed3e05aa2123cebd8cc7a75ee28fc
kangsudal-pc1 <kangsudal@gmail.com>
2021-05-01 12:36:19 +0900

    add todo list

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 
-class TodoList extends StatelessWidget{
+class TodoList extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Text(
+                "All todos",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Divider(),
+              SizedBox(height: 20),
+              ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text("Todo title"),
+                  );
+                },
+                shrinkWrap: true,
+                itemCount: 5,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
