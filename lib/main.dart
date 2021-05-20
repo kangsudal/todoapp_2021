4302ee1fd840cabef5528d83a3eb7ff886b1da3d
kangsudal-pc1 <kangsudal@gmail.com>
2021-05-20 17:10:05 +0900

    cloud_firestore, firebase_core, FutureBuilder, multiDex 사용자설정코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/todo_list.dart';
+
+import 'loading.dart';
 
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TodoList(),
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey[900],
-        primarySwatch: Colors.teal,
-      ),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError){
+          return Scaffold(
+            body: Center(child: Text(snapshot.error),),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting){
+          return Loading();
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: TodoList(),
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.grey[900],
+            primarySwatch: Colors.teal,
+          ),
+        );
+      }
     );
   }
 }
